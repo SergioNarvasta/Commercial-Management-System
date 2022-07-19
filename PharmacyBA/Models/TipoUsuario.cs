ee23dda13ddeb079cfdd5af791da55c9c07d4f93
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-18 19:47:57 -0500

    Create Models

--- PharmacyBA/Models/TipoUsuario.cs
+++ PharmacyBA/Models/TipoUsuario.cs
@@ -1,4 +1,10 @@
-﻿namespace PharmacyBA.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyBA.Models
 {
     public class TipoUsuario
     {
---
+++ PharmacyBA/Models/Usuario .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyBA.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Idusuario { get; set; }
+
+        public string Apellidos { get; set; }
+        public string Nombres   { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string DNI { get; set; }
+        public boolean Estado { get; set; }
+        public Date FechaIng { get; set; } 
+
+
+
+             Tipo
+
+    }
+}
