e0a46b9cc5b04eba78b6cd7da6358863c785e569
PierMauricio <pier.maurici123@gmail.com>
2022-07-18 22:04:21 -0500

    Tipo de Comprobante

---
+++ Pharmacy/PharmacyBA/Models/TipoComprobante.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmacyBA.Models
+{
+    public class TipoComprobante
+    {
+        [Key]
+        public int IdTipoComprobante { get; set; }
+
+        [Display (Name ="Descipcion de tipo de comprobantes")]
+        [Required(ErrorMessage = "Debe de ingresar el tipo de comprobante")]
+        [MaxLength(60, ErrorMessage = "El campo no debe de tener mas de 60 caracteres")]
+        public string Descripcion { get; set; }
+
+    }
+}
