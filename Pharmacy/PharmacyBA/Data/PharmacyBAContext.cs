a631fbddc3557b5943c6af1cc525d10bff3e3cc5
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-22 20:22:00 -0500

    Config Db Context

--- Pharmacy/PharmacyBA/Data/PharmacyBAContext.cs
+++ Pharmacy/PharmacyBA/Data/PharmacyBAContext.cs
@@ -13,7 +13,23 @@ namespace PharmacyBA.Data
             : base(options)
         {
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 
-        public DbSet<PharmacyBA.Models.TipoUsuario> TipoUsuario { get; set; } = default!;
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-2NA2N4M\\SQLEXPRESS;Database=Pharmacy;Trusted_Connection=true; MultipleActiveResultSets=true;");
+
+        }
+
+        public DbSet<PharmacyBA.Models.Cliente> Cliente { get; set; }
+        public DbSet<PharmacyBA.Models.DetalleIngreso> DetalleIngreso { get; set; }
+        public DbSet<PharmacyBA.Models.Ingreso> Ingreso { get; set; }
+        public DbSet<PharmacyBA.Models.DetalleVenta> DetalleVenta { get; set; }
+        public DbSet<PharmacyBA.Models.Lote> Lote { get; set; }
+        public DbSet<PharmacyBA.Models.Producto> Producto { get; set; }
+        public DbSet<PharmacyBA.Models.Proveedor> Proveedor { get; set; }
+        public DbSet<PharmacyBA.Models.TipoComprobante> TipoComprobante { get; set; }
+        public DbSet<PharmacyBA.Models.Usuario> Usuario { get; set; }
+        public DbSet<PharmacyBA.Models.Venta> Venta { get; set; }
+        public DbSet<PharmacyBA.Models.TipoUsuario> TipoUsuario { get; set; }
     }
 }
