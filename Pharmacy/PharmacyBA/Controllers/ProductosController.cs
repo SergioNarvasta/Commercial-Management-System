5424957714f54673d567fedff8931fda7a9de16a
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-22 22:45:04 -0500

    Update Controller

---
+++ Pharmacy/PharmacyBA/Controllers/ProductosController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PharmacyBA.Data;
+using PharmacyBA.Models;
+
+namespace PharmacyBA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly PharmacyBAContext _context;
+
+        public ProductosController(PharmacyBAContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Productos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> GetProducto()
+        {
+          if (_context.Producto == null)
+          {
+              return NotFound();
+          }
+            return await _context.Producto.ToListAsync();
+        }
+
+        // GET: api/Productos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Producto>> GetProducto(int id)
+        {
+          if (_context.Producto == null)
+          {
+              return NotFound();
+          }
+            var producto = await _context.Producto.FindAsync(id);
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return producto;
+        }
+
+        // PUT: api/Productos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProducto(int id, Producto producto)
+        {
+            if (id != producto.CodProducto)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(producto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Productos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Producto>> PostProducto(Producto producto)
+        {
+          if (_context.Producto == null)
+          {
+              return Problem("Entity set 'PharmacyBAContext.Producto'  is null.");
+          }
+            _context.Producto.Add(producto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProducto", new { id = producto.CodProducto }, producto);
+        }
+
+        // DELETE: api/Productos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProducto(int id)
+        {
+            if (_context.Producto == null)
+            {
+                return NotFound();
+            }
+            var producto = await _context.Producto.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Producto.Remove(producto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductoExists(int id)
+        {
+            return (_context.Producto?.Any(e => e.CodProducto == id)).GetValueOrDefault();
+        }
+    }
+}
