7bc4f0576ceb04c3cae2794c587c177a97345664
luandece <luan06dc99@gmail.com>
2022-08-02 21:20:16 -0500

    Comentario

--- Pharmacy/PharmacyBA/Data/PharmacyBAContext.cs
+++ Pharmacy/PharmacyBA/Data/PharmacyBAContext.cs
@@ -16,8 +16,7 @@ namespace PharmacyBA.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-2NA2N4M\\SQLEXPRESS;Database=Pharmacy;Trusted_Connection=true; MultipleActiveResultSets=true;");
-
+            optionsBuilder.UseSqlServer("Server=LAPTOP-6CK5JQAF\\LUAN;Database=Pharmacy;Trusted_Connection=true; MultipleActiveResultSets=true;");
         }
 
         public DbSet<PharmacyBA.Models.Cliente> Cliente { get; set; }
