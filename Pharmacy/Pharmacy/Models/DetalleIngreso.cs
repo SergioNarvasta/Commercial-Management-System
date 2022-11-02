1f1ec859563dfbb919b3b31c1bab0c8515ed833a
Sergio Narvasta <snarvasta@hotmail.com>
2022-11-02 14:57:18 -0500

    Create Models in PharmacyMVC

---
+++ Pharmacy/Pharmacy/Models/DetalleIngreso.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmacyBA.Models
+{
+    public class DetalleIngreso
+    {
+        [Key]
+        public int CodDetIng { get; set; }
+        public int Cantidad { get; set; }
+        public string ?UniMedida { get; set; }
+        public Double Precio { get; set; }
+        public Double SubTotal { get; set; }
+
+        //Foreign Key
+        public int CodIngreso { get; set; }
+        public virtual Ingreso ?Ingreso { get; set; }
+        public int CodProducto { get; set; }
+        public virtual Producto ?Producto { get; set; }
+        public int CodProveedor { get; set; }
+        public virtual Proveedor ?Proveedor  { get; set; }
+    }
+}
---
+++ Pharmacy/Pharmacy/Models/Lote.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmacyBA.Models
+{
+    public class Lote
+    {
+        [Key]
+        public int CodLote { get; set; }
+            
+        public DateTime FechaVen { get; set; }
+        
+        public string ?RegSanit { get; set; }
+
+        public ICollection<Producto> Producto { get; set; }
+    }
+}
