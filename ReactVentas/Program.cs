8a71f4fe0d616ccc8fa2cbbbe79bdc4fd5d90e68
SergioNarvasta <sergio.narvasta@itsight.pe>
2023-03-11 10:02:26 -0500

    Se agregio la opcion Buscar

--- ReactVentas/Program.cs
+++ ReactVentas/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddControllers().AddJsonOptions(option =>
     option.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
